2295571b1a194a62429ed193d55de6a7c3f14246
kaan <uepef@student.kit.edu>
2025-12-16 04:22:41 +0300

    feat: compete dele_student function.

--- student_grader/services/student_service.py
+++ student_grader/services/student_service.py
@@ -6,6 +6,9 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 FOLDER_DIR = CURRENT_DIR[:len(CURRENT_DIR) - 1 - len("services")]
 TARGET_DIR = os.path.join(FOLDER_DIR, "data", "students.csv")
 
+STUDENT_ALREADY_EXIST = "Student with the ID {student_id} already exists"
+DATABASE_EMPTY = "The database is empty please add a student before deleting it."
+
 
 def is_file_empty(path):
     if os.path.getsize(path) > 0:
@@ -14,7 +17,7 @@ def is_file_empty(path):
         return False
 
 def add_student(*, student_id, non_exam_score, exam_score):
-    if is_file_empty(TARGET_DIR):
+    if not is_file_empty(TARGET_DIR):
         df = pd.read_csv(TARGET_DIR)
     else:
         df = pd.DataFrame({
@@ -31,7 +34,9 @@ def add_student(*, student_id, non_exam_score, exam_score):
             is_matched = True
     
     if is_matched:
-        raise StudentAlreadyExistsError(f"Student with the ID {student_id} already exists")
+        raise StudentAlreadyExistsError(STUDENT_ALREADY_EXIST.format(
+            student_id=student_id
+        ))
     else:
         new_student = {
             "student_id": student_id,
@@ -39,10 +44,14 @@ def add_student(*, student_id, non_exam_score, exam_score):
             "exam_score": exam_score
         }
         df = pd.concat([df, pd.DataFrame([new_student])], ignore_index=True)
+        df.to_csv(TARGET_DIR, index=False)
 
 
 def delete_student(studnet_id):
-    if is_file_empty(TARGET_DIR):
-        raise EmptyDatabaseError("The database is empty please add a student before deleting it.")
-
+    if not is_file_empty(TARGET_DIR):
+        raise EmptyDatabaseError(DATABASE_EMPTY)
+    else:
+        df = pd.read_csv(TARGET_DIR)
+        df = df[df["student_id"] != studnet_id]
+        df.to_csv(TARGET_DIR, index=False)
 
