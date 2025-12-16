a66d8be4b9ebeebb777dcb03097ca828c2cb893c
kaan <uepef@student.kit.edu>
2025-12-16 05:02:31 +0300

    feat: add a function to import studens from an excel file into the database.

--- student_grader/services/student_service.py
+++ student_grader/services/student_service.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from student_grader.exceptions import StudentAlreadyExistsError, EmptyDatabaseError
+from student_grader.exceptions import StudentAlreadyExistsError, EmptyDatabaseError, ExcelError
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 FOLDER_DIR = CURRENT_DIR[:len(CURRENT_DIR) - 1 - len("services")]
@@ -10,14 +10,13 @@ TARGET_DIR = os.path.join(FOLDER_DIR, "data", "students.csv")
 STUDENT_ALREADY_EXIST = "Student with the ID {student_id} already exists"
 DATABASE_EMPTY = "The database is empty please add a student before deleting it."
 
-
 def is_file_empty(path):
     if os.path.getsize(path) > 0:
         return True
     else:
         return False
 
-def add_student(*, student_id, non_exam_score, exam_score):
+def add_student(*, student_id, non_exam_score: float, exam_score: float):
     if not is_file_empty(TARGET_DIR):
         df = pd.read_csv(TARGET_DIR)
     else:
@@ -56,3 +55,17 @@ def delete_student(studnet_id):
         df = df[df["student_id"] != studnet_id]
         df.to_csv(TARGET_DIR, index=False)
 
+
+def import_students_from_excel(file_path: str):
+    try:
+        df_excel = pd.read_excel(file_path)
+    except (FileNotFoundError, ValueError) as e:
+        raise ExcelError("Invalid excel file") from e
+    
+    if not is_file_empty(TARGET_DIR):
+        df_excel.to_csv(TARGET_DIR)
+        return
+    else:
+        df_csv = pd.read_csv(TARGET_DIR)
+        df_combined = pd.concat([df_csv, df_excel], ignore_index=True)
+        df_combined.to_csv(TARGET_DIR, index=False)
