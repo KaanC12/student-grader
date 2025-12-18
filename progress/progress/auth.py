e7f423533066fdf3d0b726a72a2105103b78fdea
kaan <uepef@student.kit.edu>
2025-12-18 05:20:47 +0300

    refactor: code design was changed a bit. the repo student_grader will be using as a dependency.

---
+++ progress/progress/auth.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+KEY_PATH = os.path.join(BASE_PATH, "..", "..", "student_grader", "data", "client.csv")
+
+
+def check_api_key(api_key, name):
+    if not os.path.exists(KEY_PATH) or os.path.getsize(KEY_PATH) == 0:
+        return False
+    
+    df = pd.read_csv(KEY_PATH, index_col="api_key")
+
+    if api_key not in df.index:
+        return False
+    
+    if df.loc[api_key, "client_name"] != name:
+        return False
+    
+    df.loc[api_key, "active"] = True
+    df.to_csv(KEY_PATH)
+    return True
